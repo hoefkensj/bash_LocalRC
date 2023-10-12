d69d2d1a303d4942931ffdf17ef589004109bae0
hoefkensj <hoefkens.j@gmail.com>
2023-10-12 21:26:00 +0200

    install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 # ############################################################################
-# # PATH: bash_scripts/Bash_LocalRC  AUTHOR: Hoefkens.J@gmail.com
+# # PATH: bash_scripts/bash_LocalRC        AUTHOR: Hoefkens.J@gmail.com
 # # FILE: install.sh                                   0v7 - 2023.04.26
 # ############################################################################
 #
-INSTALLDIR="/opt/local/config/rc/"
-NAME="bash"
-INSTALLPATH="$INSTALLDIR/$NAME"
-mkdir -p $INSTALLPATH
-cp -rvf "src/" "$INSTALLPATH"
-cd $INSTALLPATH
-ls
-unset latest INSTALLDIR NAME INSTALLPATH 
+PROJECT="bash_LocalRC"
+INSTALLDIR="/opt/local/config/rc/bash"
+mkdir -p $INSTALLDIR
+cp -v "$PWD/src/bash_LocalRC/"* $INSTALLDIR
+sudo touch "/etc/bash/bashrc.d/localRC"
+sudo chmod 777 "/etc/bash/bashrc.d/localRC"
+echo "sourcepath  $INSTALLDIR" >> "/etc/bash/bashrc.d/localRC"
+source "/etc/bash/bashrc.d/localRC"
