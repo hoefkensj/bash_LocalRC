cf688514eae17f70f7b151173e5543def2936105
hoefkensj <hoefkens.j@gmail.com>
2023-11-17 15:09:48 +0100

    0, 23118, 23119, install.sh, 501_history.conf, and 611_aliases_gentoo.conf

--- install.sh
+++ install.sh
@@ -7,8 +7,11 @@
 PROJECT="bash_LocalRC"
 INSTALLDIR="/opt/local/config/rc/bash"
 mkdir -p $INSTALLDIR
-cp -v "$PWD/src/bash_LocalRC/"* $INSTALLDIR
+cp -v "$PWD/src/"* $INSTALLDIR
 sudo touch "/etc/bash/bashrc.d/localRC"
 sudo chmod 777 "/etc/bash/bashrc.d/localRC"
-echo "sourcepath  $INSTALLDIR" >> "/etc/bash/bashrc.d/localRC"
+if [[ $( type -t sourcepath ) != "function" ]] ; then
+	match=$(cat  "/etc/bash/bashrc.d/localRC"| rg   "sourcepath  $INSTALLDIR" )
+	[[ -z "$match" ]] && echo "sourcepath  $INSTALLDIR" >> "/etc/bash/bashrc.d/localRC"
+fi
 source "/etc/bash/bashrc.d/localRC"
